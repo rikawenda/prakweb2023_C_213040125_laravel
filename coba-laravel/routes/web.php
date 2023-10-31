604f5b1551151c3b949c5a88ee3046e4e51bdea1
rikawenda <febriyantirika081@gmail.com>
2023-10-31 11:52:15 +0700

    Video 3: Struktur Folder, Routes  & View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rika Febriyanti E. Wenda",
+        "email" => "rika14322@gmail.com",
+        "image" => "wenda.jpg"
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view('posts');
 });
