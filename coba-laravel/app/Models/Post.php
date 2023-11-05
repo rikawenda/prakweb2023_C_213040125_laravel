339555a9805577f69dae96885441965d960b3e8e
rikawenda <febriyantirika081@gmail.com>
2023-11-05 22:07:41 +0700

    Video 6: Post Model

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,45 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-   private static $blog_posts = [
-    [
-        [
-            "title" => "Pelet Manjur Abang Geprek Bajak",
-            "slug" => "judul-post-pertama",
-            "author" => "Wenda",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, quam blanditiis ut odit dolor dolorem quos praesentium culpa saepe incidunt voluptas nobis adipisci quae neque totam earum minus aperiam ad distinctio numquam! Esse culpa vitae nisi accusantium omnis recusandae optio accusamus tempore rerum voluptatem repellat non ex nulla dolore est architecto quod tenetur, sequi impedit eveniet iure aliquam atque? Voluptatum assumenda magni, totam iste tenetur aliquid voluptatem quaerat dolorum ullam delectus ab sequi, voluptate odit temporibus velit sit quidem obcaecati quo nostrum, recusandae voluptatibus nemo alias. Distinctio aliquam cumque ipsam aut architecto, blanditiis explicabo, laudantium rerum impedit, excepturi necessitatibus voluptatem!"
-        ],
-        [
-            "title" => "Cintaku Kesruduk Peternak Banteng",
-            "slug" => "judul-post-kedua",
-            "author" => "Deby",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, quam blanditiis ut odit dolor dolorem quos praesentium culpa saepe incidunt voluptas nobis adipisci quae neque totam earum minus aperiam ad distinctio numquam! Esse culpa vitae nisi accusantium omnis recusandae optio accusamus tempore rerum voluptatem repellat non ex nulla dolore est architecto quod tenetur, sequi impedit eveniet iure aliquam atque? Voluptatum assumenda magni, totam iste tenetur aliquid voluptatem quaerat dolorum ullam delectus ab sequi, voluptate odit temporibus velit sit quidem obcaecati quo nostrum, recusandae voluptatibus nemo alias. Distinctio aliquam cumque ipsam aut architecto, blanditiis explicabo, laudantium rerum impedit, excepturi necessitatibus voluptatem!"
-        ],
-        [
-            "title" => "M&V Cinta Mati Abadi 100%",
-            "slug" => "judul-post-ketuga",
-            "author" => "Vina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, quam blanditiis ut odit dolor dolorem quos praesentium culpa saepe incidunt voluptas nobis adipisci quae neque totam earum minus aperiam ad distinctio numquam! Esse culpa vitae nisi accusantium omnis recusandae optio accusamus tempore rerum voluptatem repellat non ex nulla dolore est architecto quod tenetur, sequi impedit eveniet iure aliquam atque? Voluptatum assumenda magni, totam iste tenetur aliquid voluptatem quaerat dolorum ullam delectus ab sequi, voluptate odit temporibus velit sit quidem obcaecati quo nostrum, recusandae voluptatibus nemo alias. Distinctio aliquam cumque ipsam aut architecto, blanditiis explicabo, laudantium rerum impedit, excepturi necessitatibus voluptatem!"
-        ],
-        ]
-    ];
-
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug) {
-        $posts = static::all();
-        //$post = [];
-        //foreach($blog_posts as $p) {
-          //if($p["slug"] === $slug) {
-            //$post = $p;
-            //}
-       //}
-
-       return $post->firstWhere('slug', $slug);
-    }
+    use HasFactory;
+    
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,11 +2,9 @@
 
 @section('container')
 
-<article>
-    <h2>{{ $post["title"] }}</h2>
-    <h5>{{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
-</article>
+<h1 class ="mb-5">{{ $post->title }}</h1>
+
+{!!  $post->body  !!}
 
 <a href="/posts">Back to Post</a>
 @endsection
