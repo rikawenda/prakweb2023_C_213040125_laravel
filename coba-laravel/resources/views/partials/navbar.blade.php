065ac046f8354b74c1769c2053ef21e54aba06b8
rikawenda <febriyantirika081@gmail.com>
2023-11-12 03:15:50 +0700

    Video 12: Redesign UI

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-secondary">
+<nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container">
     <a class="navbar-brand" href="/">WPU BLOG</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,13 +7,16 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "Home") ? 'active' : ''}}" href="/">Home</a>
+          <a class="nav-link {{ ($active === "home") ? 'active' : ''}}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "About") ? 'active' : ''}}" href="/about">About</a>
+          <a class="nav-link {{ ($active === "about") ? 'active' : ''}}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "Posts") ? 'active' : ''}}" href="/posts">Blog</a>
+          <a class="nav-link {{ ($active === "posts") ? 'active' : ''}}" href="/posts">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active === "categories") ? 'active' : ''}}" href="/categories">Categories</a>
         </li>
       </ul>
     </div>
