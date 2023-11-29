9c05914900b8c639c4e7b43359cfdda3f64ace71
rikawenda <febriyantirika081@gmail.com>
2023-11-29 21:12:59 +0700

    Video 22: Preview, Update & Delete Image

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugServices;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -84,6 +85,7 @@ class DashboardPostController extends Controller
         $rules = ([
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ]);
 
@@ -92,6 +94,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth() -> user() -> id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -107,6 +116,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
